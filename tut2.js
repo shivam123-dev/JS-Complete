f287e8c3a14c8948763a40995fb06253286017f4
shivam123-dev <ss2249@srmist.edu.in>
2021-06-25 23:28:19 +0530

    tut2

---
+++ tut2.js
@@ -0,0 +1,39 @@
+console.time('My Code took'); // starting time count
+
+// console.log() --> It helps in printing whatever the content is as it is in the console 
+// console returns undefined 
+
+
+console.log("Hello Console! This is my tut2");
+console.log(4+34);
+console.log(34);
+console.log(true);
+console.log([1,2,3,4]); // javascript array
+console.log({shivam: 'this', marks: 34}); // javascript object
+
+// for printing objects in tabular form
+console.table({shivam:'this', marks:34});
+
+// For printing warnings: -
+console.warn('This is a warning');
+
+// For printing errors: -
+console.error("This is an error");
+
+
+console.timeEnd('My Code took'); // ending time count and displaying it on console
+
+// for clearing the console
+// console.clear();
+
+// For priting assertion error
+// console.assert(condition, error message);
+console.assert(589<189, 'This is not possible');
+
+// This is a single line comment
+
+/*
+
+    This is a multiline comment
+
+*/
