0688c0571d4978b119f6029d9651ba42a16b1b2b
shivam123-dev <ss2249@srmist.edu.in>
2021-07-01 14:50:44 +0530

    Sample Excersice - Manipulating DOM with JavaScript

---
+++ ex.js
@@ -0,0 +1,9 @@
+let myHeading = 'This is my heading';
+function disp() {
+    document.body.innerHTML = `
+        <h1> This is my heading created with the help of Javascript </h1>
+        <p> This is my paragraph created with the help of Javascript </p>
+    `
+}
+
+disp();
