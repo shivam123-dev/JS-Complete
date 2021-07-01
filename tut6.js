ea1b306196b46e0d78f627c6b5995e2468a8bf7e
shivam123-dev <ss2249@srmist.edu.in>
2021-07-01 14:35:37 +0530

    tut6 - Strings: Properties"

--- tut6.js
+++ tut6.js
@@ -1,27 +1,27 @@
 console.log('This is tut6');
 const name = 'Shivam';
 const greeting = 'Good Morning';
-// console.log(greeting + ' ' + name);
+console.log(greeting + ' ' + name);
 
 let html;
 html = "<h1> This is heading</h1>" +
     "<p> This is my para</p>"
 // Alternative of doing "+" everytime before adding a new string.
 html = html.concat('this'); // it joins two or more strings
-// console.log(html);
+console.log(html);
 
 // prints the length of the string
-// console.log('Length of the string:',html.length);
+console.log('Length of the string:',html.length);
 
 // for converting whole string to lower case we can use: - 
-// console.log(html.toLowerCase());
+console.log(html.toLowerCase());
 // for converting whole string to upper case we can use: - 
-// console.log(html.toUpperCase());
+console.log(html.toUpperCase());
 // this will print the original string beccause we didn't update the string
-// console.log(html);
+console.log(html);
 
 // string slicing
-// console.log(html[0]); // prints the first character
+console.log(html[0]); // prints the first character
 
 // prints the starting index of the string
 console.log(html.indexOf('h1'));
@@ -36,3 +36,18 @@ console.log(html.charAt(0));
 // want to check something
 console.log(html.endsWith('this'));
 console.log(html.endsWith('sleriwaer'));
+
+// to print the substring from starting Index to ending index-1
+console.log(html.substring(0,3));
+
+// to print the last 4 characters
+console.log(html.slice(-4));
+// to print the starting 4 characters
+console.log(html.slice(0,4));
+
+// for splitting the string into elements
+console.log(html.split(' '));
+console.log(html.split('>'));
+
+// for replacing the first occurence of the string
+console.log(html.replace('this', 'it'));
