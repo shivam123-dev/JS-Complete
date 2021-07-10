e533f21b0f86f66d7ec04949ed168c95eba46d4c
shivam123-dev <ss2249@srmist.edu.in>
2021-07-10 15:00:22 +0530

    Variables and Comments in Javascript

---
+++ VariablesandComments.js
@@ -0,0 +1,18 @@
+// Javascript Variables and Comments
+
+// Comments are ignored by the all language compilers and interpreters
+
+// This is a single line comment
+
+/*
+multi
+line
+comment
+*/
+// Containers to store data values are called as Variables
+
+let num1 = 34;
+let num2 = 56;
+console.log(num1 + num2);
+
+
