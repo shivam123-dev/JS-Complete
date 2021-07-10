a09b34a029da39f6985723750652ea973a411bc7
shivam123-dev <ss2249@srmist.edu.in>
2021-07-10 14:57:03 +0530

    Conditionals in Javascript

---
+++ Conditionals.js
@@ -0,0 +1,31 @@
+// Conditionals in Javascript
+
+// let age = 34;
+let age = 3;
+// Single if statement
+if (age > 18) {
+    console.log('You cannot drink water');
+}
+// If-else statement
+if (age > 18) {
+    console.log('You cannot drink water');
+}
+else{
+    console.log('You can drink');
+}
+
+// If-else Ladder
+age = 1
+if (age > 32) {
+    console.log('You are not a kid');
+}
+else if(age > 26){
+    console.log('Bachhe nhi rahe');
+}
+else if(age > 18){
+    console.log('Bachhe');
+}
+else{
+    console.log('Nhi rahe');
+}
+console.log('End of ladder');
