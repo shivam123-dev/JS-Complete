2b1f836789f69215f34d201bc8cbfd27e0642c40
shivam123-dev <ss2249@srmist.edu.in>
2021-07-10 14:59:26 +0530

    Functions in Javascript

---
+++ Functions.js
@@ -0,0 +1,12 @@
+// Functions in Javascript
+
+// DRY (Principal) = Do not repeat yourself
+function avg(a, b) {
+    // return (a + b) / 2;
+    let c = (a + b) / 2;
+    return c;
+}
+
+let c1 = avg(4, 6);
+let c2 = avg(14, 16);
+console.log(c1, c2);
