ae42443ec8cbad43504d06b4efa3586bba0024cc
shivam123-dev <ss2249@srmist.edu.in>
2021-07-10 15:01:38 +0530

    Operators in Javascript

---
+++ Operators.js
@@ -0,0 +1,49 @@
+// Operators in Javascript
+
+// 1. Arithmetic Operators
+let a = 34;
+let b = 54;
+console.log(a + b);
+console.log(a - b);
+console.log(a * b);
+console.log(a / b);
+
+// 2. Assignment Operators->Used for assiging the values 
+let c = b;
+console.log(c);
+c += 2;
+console.log(c);
+c -= 2;
+console.log(c);
+c *= 2;
+console.log(c);
+c /= 2;
+console.log(c);
+
+
+// 3. Comparison Operators
+let x = 34;
+let y = 23;
+
+console.log(x == y);
+console.log(x <= y);
+console.log(x >= y);
+console.log(x != y);
+console.log(x > y);
+console.log(y > x);
+
+// 4. Logical Operators -> operate on boolean values
+
+// 1) Logical AND
+console.log(true && false);
+console.log(true && true);
+console.log(false && false);
+console.log(false && true);
+// 2) Logical OR
+console.log(true || false);
+console.log(true || true);
+console.log(false || true);
+console.log(false || false);
+// 3) Logical NOT
+console.log(!false);
+console.log(!true);
