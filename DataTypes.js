19cd868f941198c369b454ec1f744170760f6e52
shivam123-dev <ss2249@srmist.edu.in>
2021-07-10 14:57:52 +0530

    DataTypes in Javascript

---
+++ DataTypes.js
@@ -0,0 +1,49 @@
+// Data Types in Javascript
+
+// 1. String
+let str1 = "This is a string";
+let str2 = 'This is also a string';
+// 2. Number
+let num1 = 455;
+let num2 = 56.65;
+// 3. Objects
+// key : value pairs
+let marks = {
+    ravi: 34,
+    shubham: 24,
+    shivam: 23
+};
+// 4. Boolean
+let a = true;
+let b = false;
+// 5. Undefined -> no value stored in und
+// let und = undefined;
+let und;
+// 6. NULL -> we define no value stored
+let nu = null;
+// 7. Arrays-> Collection of elements
+
+let arr = [1, 2, 3, 4, 5, "baloo bhai"];
+console.log(arr);
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
+console.log(arr[3]);
+console.log(arr[4]);
+console.log(arr[5]);
+
+
+
+console.log(str1);
+console.log(str2);
+console.log(num1);
+console.log(num2);
+console.log(marks);
+console.log(a);
+console.log(b);
+console.log(und);
+console.log(nu);
+
+// At a very high level, there are 2 types of data types in js : -
+// 1. Primitive Data types-> undefined, null, string, boolean, symbol
+// 1. Reference Data types-> 
