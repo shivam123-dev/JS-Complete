70cd58dcebac9ff399b5ace3b6774a71b9a8eaf5
shivam123-dev <ss2249@srmist.edu.in>
2021-07-10 15:01:10 +0530

    Getting Started With JavaScript

---
+++ Getting Started With JavaScript.js
@@ -0,0 +1,13 @@
+// 1. Ways to print in Javascript
+// console.log("Hello World");
+// alert("me");
+// document.write("This is me using document.write()");
+
+// 2. Javascript Console API (Application Programming Interface)
+console.log("Hello World");
+// This displays an error
+console.error('This is an error');
+
+// This displays a warning
+console.warn('This is a warning');
+console.log('Hello World', 4+6, "another string");
