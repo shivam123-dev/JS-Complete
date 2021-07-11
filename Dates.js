57b5e8932e2fe220069626ac5430464352d9546c
shivam123-dev <ss2249@srmist.edu.in>
2021-07-11 11:48:27 +0530

    Dates in Javascript

---
+++ Dates.js
@@ -0,0 +1,9 @@
+// Dates in Javascript
+let myDate = new Date();
+console.log(myDate);
+console.log(myDate.getTime());
+console.log(myDate.getFullYear());
+console.log(myDate.getDay());
+console.log(myDate.getHours());
+console.log(myDate.getMinutes());
+console.log(myDate.getSeconds());
